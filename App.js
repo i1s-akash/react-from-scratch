4e33dfd7dbcfe1604b7e5422415ed030ac987f5b
Akash Prajapati <66644705+i1s-akash@users.noreply.github.com>
2023-04-22 17:12:39 +0000

    2-Day

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parentId" }, [
   React.createElement("div", { id: "firstChild" }, [
     React.createElement("h1", { id: "firstH1" }, "First H1"),
@@ -8,5 +11,6 @@ const parent = React.createElement("div", { id: "parentId" }, [
     React.createElement("h1", { id: "thirdH1" }, "Fourth H1"),
   ]),
 ]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
